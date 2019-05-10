41b35e5ea53e8849c74c23b66e7e9d8ced301073
Isaac Elliott <isaace71295@gmail.com>
2019-05-10 11:11:13 +1000

    cleaned up

--- src/Adapton.hs
+++ src/Adapton.hs
@@ -2,7 +2,16 @@
 {-# language GeneralizedNewtypeDeriving #-}
 {-# language DataKinds, PolyKinds, UnboxedTuples, UndecidableInstances #-}
 {-# language TypeApplications #-}
-module Adapton where
+module Adapton
+  ( runA, runAIO
+    -- * Thunks
+  , Thunk, thunk, force
+    -- * Refs
+  , Ref, ref, getRef, setRef
+    -- * AVars
+  , AVar, avar, getAVar, setAVar
+  )
+where
 
 import Control.Concurrent.Supply (Supply, newSupply, freshId)
 import Control.Monad ((<=<), when)
@@ -21,38 +30,39 @@ import Data.Primitive.MutVar (MutVar, newMutVar, readMutVar, writeMutVar, modify
 import qualified Data.HashTable.ST.Basic as HashTable
 import qualified Data.Set as Set
 
-data SomeAThunk m where
-  SomeAThunk :: AThunk m a -> SomeAThunk m
-instance Eq (SomeAThunk m) where
-  SomeAThunk a == SomeAThunk b = liftEq undefined a b
-instance Ord (SomeAThunk m)  where
-  compare (SomeAThunk a) (SomeAThunk b) = liftCompare undefined a b
+data SomeThunk m where
+  SomeThunk :: Thunk m a -> SomeThunk m
+instance Eq (SomeThunk m) where
+  SomeThunk a == SomeThunk b = liftEq undefined a b
+instance Ord (SomeThunk m)  where
+  compare (SomeThunk a) (SomeThunk b) = liftCompare undefined a b
 
-data AThunk m a
-  = AThunk
+-- | `U a`
+data Thunk m a
+  = Thunk
   { _id :: Int
   , _thunk :: A m a
   , _result :: MutVar (PrimState m) a
-  , _sub :: MutVar (PrimState m) (Set (SomeAThunk m))
-  , _super :: MutVar (PrimState m) (Set (SomeAThunk m))
+  , _sub :: MutVar (PrimState m) (Set (SomeThunk m))
+  , _super :: MutVar (PrimState m) (Set (SomeThunk m))
   , _clean :: MutVar (PrimState m) Bool
   }
 
-instance Hashable (AThunk m a) where
+instance Hashable (Thunk m a) where
   hash = _id
   hashWithSalt a = hashWithSalt a . _id
 
 data Env m
   = Env
   { _supply :: MutVar (PrimState m) Supply
-  , _current :: MutVar (PrimState m) (Maybe (SomeAThunk m))
+  , _current :: MutVar (PrimState m) (Maybe (SomeThunk m))
   }
 
-instance Eq (AThunk m a) where; a == b = _id a == _id b
-instance Eq1 (AThunk m) where; liftEq _ a b = _id a == _id b
-instance Ord (AThunk m a) where
+instance Eq (Thunk m a) where; a == b = _id a == _id b
+instance Eq1 (Thunk m) where; liftEq _ a b = _id a == _id b
+instance Ord (Thunk m a) where
   compare a b = compare (_id a) (_id b)
-instance Ord1 (AThunk m) where
+instance Ord1 (Thunk m) where
   liftCompare _ a b = compare (_id a) (_id b)
 
 newtype A m a = A { unA :: ReaderT (Env m) m a }
@@ -64,12 +74,12 @@ fresh ref = do
   let (n, a') = freshId a
   n <$ writeMutVar ref a'
 
-mkAThunk ::
+thunk ::
   forall m a.
   PrimBase m =>
   A m a ->
-  A m (AThunk m a)
-mkAThunk a = do
+  A m (Thunk m a)
+thunk a = do
   supplyRef <- A $ asks _supply
   n <- A . liftPrim @m $ fresh supplyRef
   resultRef <- A . liftPrim @m $ newMutVar undefined
@@ -77,7 +87,7 @@ mkAThunk a = do
   superRef <- A . liftPrim @m $ newMutVar mempty
   cleanRef <- A . liftPrim @m $ newMutVar False
   pure $
-    AThunk
+    Thunk
     { _id = n
     , _thunk = a
     , _result = resultRef
@@ -86,35 +96,36 @@ mkAThunk a = do
     , _clean = cleanRef
     }
 
-addDcgEdge :: forall m a b. PrimBase m => AThunk m a -> AThunk m b -> A m ()
+addDcgEdge :: forall m a b. PrimBase m => Thunk m a -> Thunk m b -> A m ()
 addDcgEdge super sub = do
-  liftPrim @m . modifyMutVar (_sub super) $ Set.insert (SomeAThunk sub)
-  liftPrim @m . modifyMutVar (_super sub) $ Set.insert (SomeAThunk super)
+  liftPrim @m . modifyMutVar (_sub super) $ Set.insert (SomeThunk sub)
+  liftPrim @m . modifyMutVar (_super sub) $ Set.insert (SomeThunk super)
 
-delDcgEdge :: forall m a b. PrimBase m => AThunk m a -> AThunk m b -> A m ()
+delDcgEdge :: forall m a b. PrimBase m => Thunk m a -> Thunk m b -> A m ()
 delDcgEdge super sub = do
-  liftPrim @m . modifyMutVar (_sub super) $ Set.delete (SomeAThunk sub)
-  liftPrim @m . modifyMutVar (_super sub) $ Set.delete (SomeAThunk super)
+  liftPrim @m . modifyMutVar (_sub super) $ Set.delete (SomeThunk sub)
+  liftPrim @m . modifyMutVar (_super sub) $ Set.delete (SomeThunk super)
 
-compute :: forall m a. PrimBase m => AThunk m a -> A m a
+compute :: forall m a. PrimBase m => Thunk m a -> A m a
 compute a = do
   b <- liftPrim @m $ readMutVar (_clean a)
   if b
     then liftPrim @m $ readMutVar (_result a)
     else do
-      traverse_ (\(SomeAThunk b) -> delDcgEdge a b) =<< liftPrim @m (readMutVar $ _sub a)
+      traverse_ (\(SomeThunk b) -> delDcgEdge a b) =<< liftPrim @m (readMutVar $ _sub a)
       liftPrim @m $ writeMutVar (_clean a) True
       liftPrim @m . writeMutVar (_result a) =<< _thunk a
       compute a
 
-dirty :: forall m a. PrimBase m => AThunk m a -> A m ()
+dirty :: forall m a. PrimBase m => Thunk m a -> A m ()
 dirty a = do
   b <- liftPrim @m . readMutVar $ _clean a
   when b $ do
     liftPrim @m $ writeMutVar (_clean a) False
-    traverse_ (\(SomeAThunk x) -> dirty x) =<< liftPrim @m (readMutVar $ _super a)
+    traverse_ (\(SomeThunk x) -> dirty x) =<< liftPrim @m (readMutVar $ _super a)
 
-newtype Ref m a = Ref { unRef :: AThunk m a }
+-- | `M a`
+newtype Ref m a = Ref { unRef :: Thunk m a }
 instance Eq (Ref m a) where; Ref a == Ref b = a == b
 instance Hashable (Ref m a) where; hash = hash . unRef; hashWithSalt a = hashWithSalt a . unRef
 
@@ -128,7 +139,7 @@ ref val = do
   cleanRef <- liftPrim @m $ newMutVar True
   let
     a =
-      AThunk
+      Thunk
       { _id = n
       , _thunk = liftPrim @m $ readMutVar resultRef
       , _result = resultRef
@@ -143,14 +154,17 @@ setRef (Ref a) val = do
   liftPrim @m $ writeMutVar (_result a) val
   dirty a
 
-force :: forall m a. PrimBase m => AThunk m a -> A m a
+getRef :: PrimBase m => Ref m a -> A m a
+getRef (Ref a) = force a
+
+force :: forall m a. PrimBase m => Thunk m a -> A m a
 force a = do
   current <- A $ asks _current
   prev <- liftPrim @m $ readMutVar current
-  liftPrim @m $ writeMutVar current $ Just (SomeAThunk a)
+  liftPrim @m $ writeMutVar current $ Just (SomeThunk a)
   result <- compute a
   liftPrim @m $ writeMutVar current prev
-  result <$ traverse_ (\(SomeAThunk x) -> addDcgEdge x a) prev
+  result <$ traverse_ (\(SomeThunk x) -> addDcgEdge x a) prev
 
 memoL ::
   forall m a b.
@@ -158,15 +172,15 @@ memoL ::
   , PrimBase m
   ) =>
   (a -> A m b) ->
-  A m (a -> A m (AThunk m b))
+  A m (a -> A m (Thunk m b))
 memoL f = do
-  table :: HashTable (PrimState m) a (AThunk s b) <- liftPrim HashTable.new
+  table :: HashTable (PrimState m) a (Thunk s b) <- liftPrim HashTable.new
   s <- A $ asks _supply
   pure $ \x -> do
     res <- liftPrim $ HashTable.lookup table x
     case res of
       Nothing -> do
-        a :: AThunk m b <- mkAThunk (f x)
+        a :: Thunk m b <- thunk (f x)
         a <$ liftPrim (HashTable.insert table x a)
       Just a -> pure a
 
@@ -179,18 +193,18 @@ memoFix ::
   A m (a -> A m b)
 memoFix f = mfix (memo . f)
 
-newtype AVar m a = AVar { unAVar :: Ref m (AThunk m a) }
+newtype AVar m a = AVar { unAVar :: Ref m (Thunk m a) }
 instance Eq (AVar m a) where; AVar a == AVar b = a == b
 instance Hashable (AVar m a) where; hash = hash . unAVar; hashWithSalt a = hashWithSalt a . unAVar
 
 avar :: PrimBase m => A m a -> A m (AVar m a)
-avar a = fmap AVar . ref =<< mkAThunk a
+avar a = fmap AVar . ref =<< thunk a
 
-avarGet :: PrimBase m => AVar m a -> A m a
-avarGet (AVar (Ref a)) = force =<< force a
+getAVar :: PrimBase m => AVar m a -> A m a
+getAVar (AVar (Ref a)) = force =<< force a
 
-avarSet :: PrimBase m => AVar m a -> A m a -> A m ()
-avarSet (AVar v) a = setRef v =<< mkAThunk a
+setAVar :: PrimBase m => AVar m a -> A m a -> A m ()
+setAVar (AVar v) a = setRef v =<< thunk a
 
 setup :: PrimBase m => Supply -> m (Env m)
 setup sup = do
@@ -198,8 +212,8 @@ setup sup = do
   curRef <- newMutVar Nothing
   pure $ Env supRef curRef
 
-runA_ :: PrimBase m => Supply -> A m a -> m a
-runA_ sup a = setup sup >>= runReaderT (unA a)
+runA :: PrimBase m => Supply -> A m a -> m a
+runA sup a = setup sup >>= runReaderT (unA a)
 
 runAIO :: A IO a -> IO a
 runAIO a = do
